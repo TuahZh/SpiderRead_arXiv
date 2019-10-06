0d33776d194bf833b165bcd958016f8280835e27
Gao-Yuan <zgy0106@yeah.net>
2019-10-06 15:23:54 +0800

    use selenium with chromedriver

--- papers.py
+++ papers.py
@@ -491,3 +491,168 @@ class ListPapers(object):
                     self.subjects_list.append(ss)
         return self.subjects_list
 
+class PaperExt(Paper):
+    """Extend Paper class to ADS"""
+    def __init__(self, ads_id=None, arxiv_id=None, url="https://ui.adsabs.harvard.edu/abs/", nap=5.):
+        super().__init__(arxiv_id=arxiv_id, url=url, nap=nap)
+        if((ads_id is None) and (arxiv_id is None)):
+            self.ads_id = ""
+            self.reference_url = ""
+            self.citation_url = ""
+        elif(arxiv_id is None):
+            self.ads_id = ads_id
+            self.link_ads = url+ads_id
+            (self.reference_url, self.citation_url) = self.get_ref_cit_url(ads_id)
+            self._find_ads_page(ads_id, self.link_ads)
+            #print(self.arxiv_id)
+        else:
+            # arxiv_id provided
+            self.get_ads_id()
+            (self.reference_url, self.citation_url) = self.get_ref_cit_url(self.ads_id)
+        pass
+    
+    def get_ads_id(self):
+        """Get the ads bibcode as an id. And get something from ads page by the way."""
+        (self.title, self.authors, self.abstract, self.date, self.std_keywords, \
+            self.doi, self.arxiv_id, self.ads_pdf_url, self.ads_id) = \
+            self._read_ads_page("",self.ads_link)
+        
+    def get_ref_cit_url(self, ads_id):
+        """Return the link to refernce and citation page of an ads_id"""
+        return (self._url_prefix+ads_id.strip()+"/references", self._url_prefix+ads_id.strip()+"/citations")
+        
+        #https://ui.adsabs.harvard.edu/abs/2010ApJ...714..320A/references
+    
+    def _find_ads_page(self, ads_id, url):
+        """Find a page on ads"""
+        (self.title, self.authors, self.abstract, self.date, self.std_keywords, \
+            self.doi, self.arxiv_id, self.ads_pdf_url, _) = \
+            self._read_ads_page(ads_id, self.link_ads, nap=self.nap_interval)
+        return
+    
+    @staticmethod
+    def _read_ads_page(ads_id, url, nap=5.):
+        """Read from an ADS abstract page."""
+        header = {'User-Agent':random.choice(user_agent_list)}
+        try:
+            source_code = requests.get(url, headers=header)
+            source_code.raise_for_status()
+        except HTTPError as e:
+            print(e)
+            return ("", [], "", "", [], "", "", "", "")
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text)
+        
+        title = soup.find("meta", {"property":"og:title"}).get("content").strip()
+        author_list = []
+        for ss in soup.findAll("meta", {"property":"article:author"}):
+            author_list.append(ss.get("content").strip())
+        ads_link = url
+        abstract = soup.find("meta", {"property":"og:description"}).get("content").strip()
+        date = soup.find("meta", {"property":"article:published_time"}).get("content").strip()
+        comments = ""
+        std_key_words_list = list(ss.get("content").strip() for ss in \
+                                  soup.findAll("meta", {"name":"citation_keywords"}))
+        doi = soup.find("meta", {"name":"citation_doi"}).get("content").strip()
+        ads_pdf_url = soup.find("meta", {"name":"citation_pdf_url"})
+        arxiv_id_sp = soup.find("a", text=re.compile("arXiv*"))
+        try:
+            arxiv_id = arxiv_id_sp.get_text().strip()
+        except AttributeError as e:
+            if(arxiv_id_sp is None):
+                arxiv_id = ""
+            else:
+                raise e
+        bibcode_sp = soup.find("i", {"title":"The bibcode is assigned by the ADS as a unique identifier for the paper."})
+        try:
+            ads_id = bibcode_sp.find_previous_sibling("a").get_text().strip()
+        except:
+            pass
+        #print(ads_id)
+        time.sleep(random.random()*nap)
+        return (title, author_list, abstract, date, std_key_words_list, doi, arxiv_id, ads_pdf_url, ads_id)
+    
+    def access_arxiv(self, change_prefix=False, use_date="ads"):
+        """Should the name be clear?"""
+        # change the url_prefix to arxiv one
+        url_pref = self._url_prefix
+        date = self.date
+        self._url_prefix = "https://arxiv.org/abs/"
+        self.search_online()
+        if(use_date=="ads"):
+            self.date = date
+        if (change_prefix):
+            return
+        else:
+            self._url_prefix = url_pref
+            return
+
+    def get_ref_cit(self, target="reference"):
+        """Return a ListPaper of the reference or citation."""
+        if (target=="reference"):
+            list_url = self.reference_url
+        elif (target=="citation"):
+            list_url = self.citation_url
+        return self.ads_list_reading(list_url, deep=False)
+
+    @staticmethod
+    def ads_list_reading(url, deep=False):
+        """Read from a list page on ADS"""
+        pass
+
+
+#def arxiv_reading(url=None, nap=5.):
+#    """Daily reading arxiv:
+#    save to a pickle or something"""
+#    if (url is None):
+#        url = "https://arxiv.org/list/astro-ph/new"
+#    header = {'User-Agent':random.choice(user_agent_list)}
+#    source_code = requests.get(url, headers=header) 
+#    #time.sleep(np.random.rand()*10)
+#    plain_text = source_code.text
+#    soup = BeautifulSoup(plain_text)
+#    time_str = soup.find("h3").get_text().strip()
+#    print(time_str)
+#    paper_list = []
+#    papers_sp = soup.find("dl")
+#    # =======================================================
+#    # Only the new papars without cross list and replacements
+#    # =======================================================
+#    #paper_list = soup.find("div", {'class': 'meta'})
+#    item_list_sp = papers_sp.findAll("dt")
+#    paper_list_sp = papers_sp.findAll("dd")
+#    
+#    if(len(item_list_sp)!=len(paper_list_sp)):
+#        print("Warning: Length different!")
+#        
+#    for tt, ss in zip(item_list_sp, paper_list_sp):
+#        paper1 = Paper()
+#        paper1.arxiv_id = tt.find("a", {"title":"Abstract"}).get_text().strip()
+#        title_sp = ss.find("div", {"class":"list-title mathjax"})
+#        title_sp.span.decompose()
+#        paper1.title = title_sp.get_text().strip()
+#        authors_sp = ss.find("div", {"class":"list-authors"})
+#        #authors_sp.span.decompose()
+#        authors_list_sp = authors_sp.findAll("a")
+#        for au in authors_list_sp:
+#            paper1.authors.append(au.get_text().strip())
+#            
+#        paper1.abstract = ss.find("p", {"class":"mathjax"}).get_text().strip()
+#        comments_sp = ss.find("div", {"class":"list-comments mathjax"})
+#        subjects_sp = ss.find("div", {"class":"list-subjects"})
+#        try:
+#            comments_sp.span.decompose()
+#            paper1.comments = comments_sp.get_text().strip()
+#        except AttributeError as e:
+#            pass
+#        subjects_sp.span.decompose()
+#        paper1.subjects = subjects_sp.get_text().strip()
+#        paper1.date = time_str
+#        #print(paper1)        
+#        paper_list.append(paper1)
+#
+#    print("Sleep...")
+#    time.sleep(random.random()*nap)
+#    print("Awake...")
+#    return paper_list, time_str
+#
